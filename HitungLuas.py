3515a465711508ad821da8e663d3cdc928a9f9d0
NouvereX <65251564+NouvereX@users.noreply.github.com>
2020-05-13 02:29:30 +0700

    Add files via upload

---
+++ HitungLuas.py
@@ -0,0 +1,88 @@
+HitungLuas()
+def display_menu():
+  raw_input("")
+  Hitung Luas()
+def HitungLuas():
+  clearScreen()
+  def persegi():
+    clearScreen():
+      print("--LUAS PERSEGI--")
+      
+      
+      sisi = input("Masukkan panjang sisi:")
+      
+      luas= sisi*sisi;
+      print"Luasnya adalah",Luas
+      
+      
+      display_menu():
+        
+def persegipanjang():
+  clearScreen():
+    print("--LUAS PERSEGI PANJANG--")
+    
+    sisi = input("Masukkan panjang:,lebar:")
+    
+    luas= panjang*lebar
+    print"Luasnya adalah",luas
+    
+    display_menu():
+      
+def segitiga():
+  clearScreen():
+    print("--LUAS SEGITIGA--")
+    
+    sisi = input('Masukkan alas:,sisi:')
+    
+    luas: alas*tinggi/2
+    print"Luasnya adalah",Luas
+     
+     display_menu():
+ 
+ def lingkaran():
+   clearScreen():
+     print("--LUAS LINGKARAN")
+     
+     sisi = input('Masukkan jari-jari:')
+     
+     luas= jari*jari*22/7
+     print"Luasnya adalah",Luas
+     
+     
+     display_menu():
+     
+        
+        
+  #MENU
+  print("---PROGRAM HITUNG LUAS")
+  print("1.Persegi")
+  print("2.Persegi Panjang")
+  print("3.Segitiga")
+  print("4.Lingkaran")
+  print("0.tutup")
+  
+  #PILIH MENU
+  menu = input("pilih menu:") 
+  
+  if menu==1:
+    persegi()
+    elif menu==2:
+      persegi panjang()
+    elif menu==3:
+      segitiga()
+    elif menu==4:
+      lingkaran()
+    elif menu==0:
+      print("bye")
+      
+ else:
+      print("Menu Tidak Ditemukan")
+      display_menu()
+      import os
+      def clearScreen():
+        os.system('clear')
+        
+
+  
+
+    
